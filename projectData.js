88ed6fa5caf47f0fc28eb839054332f3dd3291c9
Darryl Sanders <dee2wise@gmail.com>
2025-03-21 15:39:17 -0500

    added load Datat function to the script.js file
    This will load a array of data into a string template to add to the project section on the page.

---
+++ projectData.js
@@ -0,0 +1,16 @@
+    const projectData = [
+    {
+        projectname: 'Color-Generator',
+        img:'./images/profile pic.jpeg',
+        weblink:'#',
+        summary:'This is a color-generator chrome extension I made.'
+    },
+    {
+        projectname: 'Web-Notes',
+        img:'./images/profile pic.jpeg',
+        weblink:'#',
+        summary:'This is an notepad chrome extension I made.'
+    }
+]
+
+export default projectData;
---
+++ script.js
@@ -0,0 +1,21 @@
+import projectData from './projectData';
+
+function loadProjectData(data){
+
+    for(let project of data){
+        document.getElementById('projects').append( `
+            <div class="project-card">
+                <p>${project.projectname}</p>
+                <a href='${project.weblink}'<img src='${project.img}'/></a>
+                <p>${project.summary}</p>
+
+            </div>
+        
+        
+        `);
+    }
+
+    }
+
+    loadProjectData(projectData)
+    
